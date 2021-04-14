47f4fd639f46711c82e1eac56edd12f7111f2157
Danil <82463891+TerrorBladeOz@users.noreply.github.com>
2021-04-14 21:47:49 +0300

    Add files via upload

---
+++ HomeWork№1 14.04/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    //Отрывок из песни прекрасное далеко by Pyrokinesis
+    cout << "Песня Прекрасное Далёко\n\n";
+    cout << "Прекрасное далёко, не будь ко мне жестоко\nНе будь ко мне жестоко, жестоко не будь\nОт чистого истока в прекрасное далёко\nВ прекрасное далёко я начинаю путь\n\t\t\t\tPyrokinesis\n\n";
+
+    //Every hunter wants…
+    cout << "Русская версия\n\n";
+    cout << "Каждый" << endl;        //Красный
+    cout << "\tОхотник" << endl;       //Оранжевый
+    cout << "\t\tЖелает" << endl;        //Жёлтый
+    cout << "\t\t\tЗнать" << endl;         //Зелёный
+    cout << "\t\t\t\tГде" << endl;           //Голубой
+    cout << "\t\t\t\t\tСидит" << endl;         //Синий
+    cout << "\t\t\t\t\t\tФазан" << endl;          //Фиолетовый
+    cout << "English version\n\n";
+    cout << "Every" << endl;            //Красный
+    cout << "\tHunter" << endl;          //Оранжевый
+    cout << "\t\tWants" << endl;            //Жёлтый
+    cout << "\t\t\tTo know" << endl;         //Зелёный
+    cout << "\t\t\t\tWhere" << endl;          //Голубой
+    cout << "\t\t\t\t\tDoes Pheasant" << endl;  //Синий
+    cout << "\t\t\t\t\t\t\tSit\n\n" << endl;            //Фиолетовый
+    //Объявление
+    cout << "\t\t\t\t\tПродам Sony Playstation 5!!!\n";
+    cout << "\t\t\t\t\t\tНедорого!!!\n";
+    cout << "\t\t\t\t\t  Насчет цены договримся.\n";
+    cout << "\t\t\t\t\t  Но она меньше магазинной.\n";
+    cout << "\t\t\t\t\tНомер телефона: +7 987 935-44-37\n";
+    cout << "\t\t\t\t\t\t +7 \t / +7 /\n";
+    cout << "\t\t\t\t\t\t 987 \t / 987 /\n";
+    cout << "\t\t\t\t\t\t 935 \t / 935 /\n";
+    cout << "\t\t\t\t\t\t 44 \t / 44 /\n";
+    cout << "\t\t\t\t\t\t 37 \t / 37 /\n";
+    return 0;
+}
